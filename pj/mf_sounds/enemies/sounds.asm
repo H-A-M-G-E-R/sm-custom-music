a5042402a46a9b1976dc158b8fd4f4d194e11857
H-A-M-G-E-R <hamgerham@proton.me>
2025-09-05 20:02:26 -0500

    ports of mf sounds wip

---
+++ pj/mf_sounds/enemies/sounds.asm
@@ -0,0 +1,363 @@
+asar 1.91
+norom : org 0
+incsrc "../../defines.asm"
+
+; nice trick to make subnotes
+macro make_sound_subnote(note, subnote, vol, len)
+  if <subnote> < 0
+    db $F5,-<subnote>*256 : !c2
+  else
+    db $F5,<subnote>*256 : !b7
+  endif
+  <note>,<vol>,1
+  if <len> > 1
+    db $F5,0 : !b7
+    db $F0,<vol>,<len>-1
+  endif
+endmacro
+
+; Pitch calculation: round(n*0x1000/0x1053B) or round(n*4096/66875) where n is the sample rate
+
+!sampleHornoadHalzynCry = $16
+!sampleMotoYamebaSciserYardChootCry = $17
+!sampleSidehopperCry = $18
+!sampleZombieCry = $19
+!sampleHornoadSpit = $1A
+!sampleSovaHurt = $1B
+
+spcblock 6*$16+!p_instrumentTable nspc ; instruments
+  db !sampleHornoadHalzynCry,$FF,$E0,$00,$02,$84
+  db !sampleMotoYamebaSciserYardChootCry,$FF,$E0,$00,$02,$84
+  db !sampleSidehopperCry,$FF,$E0,$00,$01,$2E
+  db !sampleZombieCry,$FF,$E0,$00,$02,$84
+  db !sampleHornoadSpit,$FF,$E0,$00,$02,$84
+  db !sampleSovaHurt,$FF,$E0,$00,$01,$2F
+endspcblock
+
+spcblock 4*$16+!p_sampleTable nspc ; sample table
+  dw SampleHornoadHalzynCry,0
+  dw SampleMotoYamebaSciserYardChootCry,SampleMotoYamebaSciserYardChootCry
+  dw SampleSidehopperCry,SampleSidehopperCry
+  dw SampleZombieCry,0
+  dw SampleHornoadSpit,0
+  dw SampleSovaHurt,SampleSovaHurt
+endspcblock
+
+spcblock $B210-$6E00+!p_sampleData nspc ; sample data
+  SampleHornoadHalzynCry: incbin "hornoad_halzyn_cry_10512_noloop.brr"
+  SampleMotoYamebaSciserYardChootCry: incbin "moto_yameba_sciser_yard_choot_cry_10512_loop0.brr"
+  SampleSidehopperCry: incbin "sidehopper_dessgeega_geruda_skultera_yard_geruboss_waver_cry_4935.345_loop0.brr"
+  SampleZombieCry: incbin "zombie_cry_10512_noloop.brr"
+  SampleHornoadSpit: incbin "hornoad_spit_10512_noloop.brr"
+  SampleSovaHurt: incbin "sova_hurt_4953.705_0.brr"
+
+dw 0,0,0,0 ; padding for shared trackers
+Trackers:
+  dw .pattern0
+  dw $0000
+
+.pattern0: dw .pattern0_0, 0, 0, 0, 0, 0, 0, 0
+
+.pattern0_0
+  !endEcho
+  db 1,$7F
+  !rest
+  !end
+
+Sounds:
+  dw SoundHornoadHurt ; C0
+  dw SoundHornoadSpit ; C1
+  dw SoundHalzynHurt ; C2
+  dw SoundHalzynLunge ; C3
+  dw SoundMotoHurt ; C4
+  dw SoundZebesianHurt ; C5
+  dw SoundYamebaHurt ; C6
+  dw SoundSciserHurt ; C7
+  dw SoundGoldSciserHurt ; C8
+  dw SoundGeemerHurt ; C9
+  dw SoundZoroHurt ; CA
+  dw SoundBlueZoroHurt ; CB
+  dw SoundKihunterHurt ; CC
+  dw SoundSidehopperShakeHead ; CD
+  dw SoundSidehopperHurt ; CE
+  dw SoundDessgeegaShakeHead ; CF
+  dw SoundDessgeegaHurt ; D0
+  dw SoundReoHurt ; D1
+  dw SoundFuneHurt ; D2
+  dw SoundNamiheHurt ; D3
+  dw SoundGerudaHurt ; D4
+  dw SoundSovaHurt ; D5
+  dw SoundSkulteraSmallHurt ; D6
+  dw SoundSkulteraLargeHurt ; D7
+  dw SoundYardHurt ; D8
+  dw SoundGerubossHurt ; D9
+  dw SoundWaverHurt ; DA
+  dw SoundEvirHurt ; DB
+
+SoundHornoadHurt:
+  db $01 : dw .voice0
+.voice0
+  db !sampleHornoadHalzynCry
+  !c5,255,8
+  db $FF
+
+; but hornoads don't spit in x-fusion
+SoundHornoadSpit:
+  db $01 : dw .voice0
+.voice0
+  db !sampleHornoadSpit
+  !c5,255,4
+  db $FF
+
+SoundHalzynHurt:
+  db $01 : dw .voice0
+.voice0
+  db !sampleHornoadHalzynCry
+  !fs5,255,4
+  !as4,160,5
+  db $FF
+
+SoundHalzynLunge:
+  db $01 : dw .voice0
+.voice0
+  db !sampleHornoadHalzynCry
+  !c4,160,3
+  !g4,200,4
+  !c5,255,3
+  !c5,0,20
+  !e5,255,6
+  !e5,104,4
+  !e5,40,4
+  db $FF
+
+SoundMotoHurt:
+  db $01 : dw .voice0
+.voice0
+  db !sampleMotoYamebaSciserYardChootCry
+  !d3,160,4
+  !c4,255,4
+  !g3,160,10
+  db $FF
+
+SoundZebesianHurt:
+  db $01 : dw .voice0
+.voice0
+  db $F5,0 : !b7 ; enable legato
+  db !sampleZombieCry
+  !fs5,160,2
+  %make_sound_subnote("!c6", 3/64*12, 160, 3)
+  %make_sound_subnote("!c6", 7/64*12, 160, 2)
+  %make_sound_subnote("!c6", 5/64*12, 160, 2)
+  !c6,160,1
+  db $FF
+
+SoundYamebaHurt:
+  db $01 : dw .voice0
+.voice0
+  db !sampleMotoYamebaSciserYardChootCry
+  !f5,255,6
+  !c5,104,7
+  db $FF
+
+SoundSciserHurt:
+  db $01 : dw .voice0
+.voice0
+  db !sampleMotoYamebaSciserYardChootCry
+  !d4,255,4
+  !a3,160,4
+  !f3,104,4
+  db $FF
+
+SoundGoldSciserHurt:
+  db $01 : dw .voice0
+.voice0
+  db !sampleMotoYamebaSciserYardChootCry
+  !gs4,255,4
+  !cs4,160,4
+  !a3,104,4
+  db $FF
+
+SoundGeemerHurt:
+  db $01 : dw .voice0
+.voice0
+  db !sampleHornoadHalzynCry
+  !a5,160,4
+  !c6,104,4
+  !f6,104,4
+  db $FF
+
+SoundZoroHurt:
+  db $01 : dw .voice0
+.voice0
+  db !sampleZombieCry
+  !b3,255,5
+  !f4,160,10
+  db $FF
+
+SoundBlueZoroHurt:
+  db $01 : dw .voice0
+.voice0
+  db !sampleZombieCry
+  !ds4,255,5
+  !a4,160,10
+  db $FF
+
+SoundKihunterHurt:
+  db $01 : dw .voice0
+.voice0
+  db !sampleHornoadHalzynCry
+  !a4,160,3
+  !g5,160,3
+  !e5,160,3
+  db $FF
+
+SoundSidehopperShakeHead:
+  db $01 : dw .voice0
+.voice0
+  db !sampleSidehopperCry
+  !c4,0,8
+  !c4,200,6
+  !f4,160,3
+  !f4,120,3
+  !f4,0,7
+  !g4,120,3
+  !b4,120,3
+  db $FF
+
+SoundSidehopperHurt:
+  db $01 : dw .voice0
+.voice0
+  db !sampleSidehopperCry
+  !a5,255,5
+  !f5,224,9
+  db $FF
+
+SoundDessgeegaShakeHead:
+  db $01 : dw .voice0
+.voice0
+  db !sampleSidehopperCry
+  !e4,0,8
+  !e4,200,6
+  !a4,160,3
+  !a4,120,3
+  !a4,0,7
+  !b4,120,3
+  !d5,120,3
+  db $FF
+
+SoundDessgeegaHurt:
+  db $01 : dw .voice0
+.voice0
+  db !sampleHornoadSpit
+  !as5,255,2
+  db !sampleSidehopperCry
+  !a4,255,5
+  !c5,224,9
+  db $FF
+
+SoundReoHurt:
+  db $01 : dw .voice0
+.voice0
+  db !sampleHornoadHalzynCry
+  !d4,160,3
+  !d5,160,3
+  !a4,160,13
+  db $FF
+
+SoundFuneHurt:
+  db $01 : dw .voice0
+.voice0
+  db !sampleZombieCry
+  !b3,255,38
+  db $FF
+
+SoundNamiheHurt:
+  db $01 : dw .voice0
+.voice0
+  db !sampleZombieCry
+  !g3,255,38
+  db $FF
+
+SoundGerudaHurt:
+  db $01 : dw .voice0
+.voice0
+  db !sampleSidehopperCry
+  !c5,255,5
+  db !sampleHornoadHalzynCry
+  !f5,200,10
+  db $FF
+
+SoundSovaHurt:
+  db $01 : dw .voice0
+.voice0
+  db !sampleSovaHurt
+  !a4,255,4
+  !e5,200,4
+  db $F5,0 : !b7 ; enable legato
+  !c5,200,4
+  !c5,94,3
+  !c5,64,3
+  !c5,36,3
+  !c5,8,1
+  db $FF
+
+; TODO: it sounds bad
+SoundSkulteraSmallHurt:
+  db $01 : dw .voice0
+.voice0
+  db $F5,0 : !b7 ; enable legato
+  db !sampleSidehopperCry
+  !ds4,255,2
+  %make_sound_subnote("!a4", -29/64*12, 255, 2)
+  %make_sound_subnote("!a4", -26/64*12, 255, 2)
+  %make_sound_subnote("!a4", -23/64*12, 255, 2)
+  %make_sound_subnote("!a4", -19/64*12, 255, 2)
+  %make_sound_subnote("!a4", -13/64*12, 255, 1)
+  %make_sound_subnote("!a4", -9/64*12, 255, 3)
+  db $FF
+
+SoundSkulteraLargeHurt:
+  db $FF
+
+SoundYardHurt:
+  db $01 : dw .voice0
+.voice0
+  db !sampleMotoYamebaSciserYardChootCry
+  !f4,255,7
+  db !sampleSidehopperCry
+  !c4,200,7
+  db !sampleMotoYamebaSciserYardChootCry
+  !c4,160,5
+  db $FF
+
+SoundGerubossHurt:
+  db $01 : dw .voice0
+.voice0
+  db !sampleSidehopperCry
+  !g4,255,4
+  !g5,255,4
+  !c5,200,4
+  !d5,200,6
+  db $FF
+
+SoundWaverHurt:
+  db $01 : dw .voice0
+.voice0
+  db !sampleSidehopperCry
+  !c4,200,4
+  !f4,255,10
+  db $FF
+
+; minus the bubbles
+SoundEvirHurt:
+  db $01 : dw .voice0
+.voice0
+  db !sampleHornoadHalzynCry
+  !g4,200,4
+  db $FF
+
+endspcblock
+
+spcblock !p_extra nspc
+  dw Trackers-8 : db 0 : dw Sounds
+endspcblock execute !p_spcEngine
---
+++ pj/mf_sounds/mf_sounds.asm
@@ -0,0 +1,120 @@
+asar 1.91
+norom : org 0
+incsrc "../defines.asm"
+
+; Pitch calculation: round(n*0x1000/0x1053B) or round(n*4096/66875) where n is the sample rate
+
+!sampleMissileLaunch = $16
+!sampleMissileBombExplode = $17
+!sampleDoorOpen = $18
+!sampleDoorClose = $19
+!sampleAbsorbX0 = $1A
+!sampleAbsorbX1 = $1B
+
+spcblock 6*$16+!p_instrumentTable nspc ; instruments
+  db !sampleMissileLaunch,$FF,$E0,$00,$01,$32
+  db !sampleMissileBombExplode,$FF,$E0,$00,$01,$32
+  db !sampleDoorOpen,$FF,$E0,$00,$01,$42
+  db !sampleDoorClose,$FF,$E0,$00,$01,$42
+  db !sampleAbsorbX0,$FF,$E0,$00,$02,$81
+  db !sampleAbsorbX1,$FF,$E0,$00,$02,$80
+endspcblock
+
+spcblock 4*$16+!p_sampleTable nspc ; sample table
+  dw SampleMissileLaunch,0
+  dw SampleMissileBombExplode,0
+  dw SampleDoorOpen,0
+  dw SampleDoorClose,0
+  dw SampleAbsorbX0,SampleAbsorbX0+1024*9/16
+  dw SampleAbsorbX1,SampleAbsorbX1
+endspcblock
+
+spcblock $B210-$6E00+!p_sampleData nspc ; sample data
+  SampleMissileLaunch: incbin "missile_launch_5000_noloop.brr"
+  SampleMissileBombExplode: incbin "missile_bomb_explode_5000_noloop.brr"
+  SampleDoorOpen: incbin "door_open_5256_noloop.brr"
+  SampleDoorClose: incbin "door_close_5256_noloop.brr"
+  SampleAbsorbX0: incbin "absorb_x_0_10467.649872_1024.brr"
+  SampleAbsorbX1: incbin "absorb_x_1_10457.253504_0.brr"
+
+dw 0,0,0,0 ; padding for shared trackers
+Trackers:
+  dw .pattern0
+  dw $0000
+
+.pattern0: dw .pattern0_0, 0, 0, 0, 0, 0, 0, 0
+
+.pattern0_0
+  !endEcho
+  db 1,$7F
+  !rest
+  !end
+
+Sounds:
+  dw SoundMissileLaunch ; C0
+  dw SoundMissileExplode ; C1
+  dw SoundDoorOpen ; C2
+  dw SoundDoorClose ; C3
+  dw SoundAbsorbX ; C4
+
+SoundMissileLaunch:
+  db $01 : dw .voice0
+.voice0
+  db !sampleMissileLaunch
+  !c5,220*1.15,14
+  db $FF
+
+SoundMissileExplode:
+  db $01 : dw .voice0
+.voice0
+  db !sampleMissileBombExplode
+  !c5,200*1.15,20
+  !c5,200*36/128*1.15,20
+  !c5,200*12/128*1.15,20
+  db $FF
+
+SoundDoorOpen:
+  db $01 : dw .voice0
+.voice0
+  db !sampleDoorOpen
+  !c5,255,19
+  db $FF
+
+SoundDoorClose:
+  db $01 : dw .voice0
+.voice0
+  db !sampleDoorClose
+  !c5,255,19
+  db $FF
+
+; TODO: sounds bad
+SoundAbsorbX:
+  db $12 : dw .voice0, .voice1
+.voice0
+  db $F5,$800/9 : !gs6
+  db !sampleAbsorbX1
+  !c6,255,9
+  db $F5,$800/21 : !ds5
+  db $F0,128,21
+  db $F5,$600/13 : !a5
+  db $F0,64,13
+  db $FF
+.voice1
+  db $F5,$500/9 : !ds5
+  db !sampleAbsorbX0
+  !as4,112,9
+  db $F5,$6FF/7 : !d6
+  db $F0,224,7
+  !d6,200,5
+  !d6,0,7-5
+  !d6,104,5
+  !d6,0,8-5
+  !d6,32,5
+  !d6,0,13-5
+  db $FF
+
+endspcblock
+
+spcblock !p_extra nspc
+  dw Trackers-8 : db 0 : dw Sounds
+endspcblock execute !p_spcEngine
