c96d260cacfc9ba1320256976cbe28c964d61296
H-A-M-G-E-R <98665992+H-A-M-G-E-R@users.noreply.github.com>
2026-09-23 15:52:17 -0500

    Fix ADSR command

---
+++ pj/adsr_test_2.asm
@@ -0,0 +1,40 @@
+;../asar.exe --fix-checksum=off pj\adsr_test_2.asm pj\adsr_test_2.nspc
+;python3 build_spc.py pj\adsr_test_2.nspc
+asar 1.91
+norom : org 0
+incsrc "defines.asm"
+
+spcblock !p_songSpecificData nspc
+dw 0,0,0,0 ; padding for shared trackers
+Trackers:
+  dw Tracker2A50
+
+Tracker2A50:
+-
+  dw .pattern0
+  dw $00FF,-
+
+.pattern0: dw .pattern0_0, 0, 0, 0, 0, 0, 0, 0
+
+.pattern0_0
+  !endEcho
+  !setDPMiscCommand,!noteEndInTicks,1
+  !musicVolume,255
+  !tempo,24;24.576 or 60 bpm
+  !setDPMiscCommand,!musicTempo,147;0.576*256
+  !volume,255
+  !instr,$0B
+  db 48,$7F
+  !subloop,0
+  !adsrGain,$7F,$E0
+  !c4
+  ;!adsrGain,$80,$87
+  !adsrGain,$7F,$EF
+  !tie
+  !subloop,255
+  !end
+endspcblock
+
+spcblock !p_extra nspc
+  dw Trackers-8 : db 0
+endspcblock execute !p_spcEngine
